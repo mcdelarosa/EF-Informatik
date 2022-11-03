96c27805b9b2d69e24fb53eeea026804807b8024
Marc <marc.castellon.delarosa@gmail.com>
2022-11-03 20:15:14 +0100

    Hangman wurde noch weiterentwickelt doch wurde noch nicht abgeschlossen.

--- exercises/Hangman.py
+++ exercises/Hangman.py
@@ -1,57 +1,68 @@
 # Variablen
-gesucht = 'test'
+#import zufallsworte as zufall
+
+#wort = zufall.zufallswoerter(1)[0]  # gibt ein Zufallswort zurück
+searched = 'test'
+life = 8
+letter = None
+
+game = [searched, life, letter]
 
 gefunden = []
 falsch_geraten = []
+
+def play():
+    while not game_over():
+        inp = eingabe()
+        analise(inp)
+        print(inp)
+        show()
+    # if won():
+    #     ShowLifes()
+    # else:
+    #     game_over()
+
+
+def ShowLifes():
+    return print("Lebenspunkten: " + game[1])
+def eingabe():
+    game[2] = input('Buchstabe? ')
+    while not is_valid(game[2]):
+        ShowLifes()
+        game[2] = input('Buchstabe? ')
+    return game[2].lower()
+
+def is_valid(inp):
+    analise(game[2])
+    return True
+def analise(valid_inp):
+    if valid_inp in game[0]:
+        gefunden.append(valid_inp)
+        won()
+    else:
+        falsch_geraten.append(valid_inp)
+        game[1]= game[1] - 1
+        print(game[1])
 def show():
     print('Falsche Buchstaben:', falsch_geraten)
-    for buchstabe in gesucht:
-        if buchstabe in gefunden:
-            print(buchstabe, end=' ')
+    for game[2] in searched:
+        if game[2] in gefunden:
+            print(game[2], end=' ')
         else:
             print('_', end=' ')
     print('')
 
+def won():
 
-def is_valid(inp):
+    print("Du hast gewonnen!")
     return True
 
-def eingabe():
-    buchstabe = input('Buchstabe? ')
-    while not is_valid(buchstabe):
-        buchstabe = input('Buchstabe? ')
-    return buchstabe.lower()
-    gewonnen()
-
-
-
-def auswerten(valid_inp):
-    if valid_inp in gesucht:
-        gefunden.append(valid_inp)
-    else:
-        falsch_geraten.append(valid_inp)
-
-def gewonnen():
-    show()
-    for buchstabe in gesucht:
-        if buchstabe not in gefunden:
-            game_over()
-            return False
-    return True
 
 def game_over():
-    return False
-
-
-def play():
-    while not game_over():
-        buchstabe = eingabe()
-        auswerten(buchstabe)
-        print(buchstabe)
-        show()
-    if gewonnen():
-        print("")
+    if game[1] > 0:
+        return False
     else:
-        pass
+        print("Game Over")
+        return True
 
 play()
