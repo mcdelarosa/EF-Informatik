6571015fad48d67d775ad32886757191abb7388b
Marc <marc.castellon.delarosa@gmail.com>
2022-12-08 19:44:20 +0100

    Numtrip
    New:
    -Changes the numbers and their places (via gravitation)
    -Different colors for different numbers
    -Fill the gaps with numbers of the base 2

--- numtrip/game.py
+++ numtrip/game.py
@@ -1,19 +1,27 @@
+import math
 import random
 class MyColors:
-    GREEN = '\033[1;32m'
-    WHITE = '\033[1;37m'
-    RED = '\033[1;31m'
-    BLUE = '\033[1;34m'
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    BROWN = "\033[0;33m"
+    BLUE = "\033[0;34m"
+    PURPLE = "\033[0;35m"
+    CYAN = "\033[0;36m"
+    LIGHT_GRAY = "\033[0;37m"
+    DARK_GRAY = "\033[1;30m"
+    YELLOW = "\033[1;33m"
+    WHITE = "\033[1;37m"
 
 class Numtrip:
     space = " "
     axis_x = ["A", "B", "C", "D", "E"]
     axis_y = ["0", "1", "2", "3", "4"]
     number = 0
-    colors = [MyColors.WHITE, MyColors.RED]
+    colors = [MyColors.WHITE, MyColors.GREEN, MyColors.BLUE, MyColors.GREEN, MyColors.CYAN, MyColors.LIGHT_GRAY, MyColors.PURPLE, MyColors.BROWN, MyColors.YELLOW]
 
     def __init__(self):
 
+        self.DEBUG = False
         self.board = [
             [2, 4, 1, 8, 8],
             [4, 2, 8, 2, 1],
@@ -40,18 +48,21 @@ class Numtrip:
 
     def printTable(self):
 
-        for idx, header in enumerate([MyColors.GREEN + ' '] + Numtrip.axis_x):
+        for idx, header in enumerate([MyColors.DARK_GRAY + ' '] + Numtrip.axis_x):
             print(' ' + header, end='')
 
         print()
 
         for idx, _axis_y in enumerate(self.board):
-            print(MyColors.GREEN + ' ' + Numtrip.axis_y[idx], end='') # first column, reference board
+            print(MyColors.RED + ' ' + Numtrip.axis_y[idx], end='') # first column, reference board
             for idx2,_axis_x in enumerate(_axis_y):
+
                 if _axis_x == -1:
                     print(MyColors.RED + ' ·', end='')
-                else:
-                    print(MyColors.WHITE + ' ' + str(_axis_x), end='')
+                elif _axis_x > 0:
+                    base2 = int(math.log(_axis_x, 2))
+                    print(self.colors[base2] + ' ' + str(_axis_x), end='')
+
             print('')
 
     def fill4(self, locationValue, x, y, choosenNumber):
@@ -65,7 +76,7 @@ class Numtrip:
         if locationValue == position_value:
             if choosenNumber:
                 self.board[y][x] = self.board[y][x]*2
-                print(f' y:{y} x:{x}')
+                self.logger(f' y:{y} x:{x}')
             else:
                 self.board[y][x] = -1
 
@@ -78,7 +89,7 @@ class Numtrip:
         bottom = len(self.board)
         for x in range(len(self.board[0])):
             for y in reversed(range(bottom)):
-                # print(f" ... {x} . {y}")
+                self.logger(f" ... {x} . {y}")
                 if self.board[y][x] == -1 and y > 0:
                     nextUpValue = self.getFirstUpNumber(y, x)
                     if nextUpValue != None:
@@ -86,19 +97,23 @@ class Numtrip:
                         self.board[nextUpValue["y"]][x] = -1
             for y in reversed(range(bottom)):
                 if self.board[y][x] == -1 and y >= 0:
-                    self.board[y][x] = random.randint(1, 8)
+                    self.board[y][x] = 2**random.randint(0, 3)
 
     def getFirstUpNumber(self, pos_y, pos_x):
         # print(f" ... ... y = {pos_y} , x= {pos_x}")
         for y in reversed(range(pos_y)):
             value = self.board[y][pos_x]
-            # print(f" ... ... ... {value}")
+            self.logger(f" ... ... ... {value}")
             if value != -1:
                 return {"y": y, "value": value }
         return None
 
+    def logger(self, message):
+        if self.DEBUG:
+            print(message)
 
 game = Numtrip()
+game.DEBUG = False
 
 while True:
     game.printTable()
